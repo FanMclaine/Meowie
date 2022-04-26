8aadd4df4a97992a15f3694f73f604d2ebd5054b
Mclnoot_ <66262586+FanMclaine@users.noreply.github.com>
2022-04-26 09:34:41 +0000

    voltage help test

--- cogs/animals.py
+++ cogs/animals.py
@@ -8,33 +8,35 @@ def setup(client) -> commands.Cog:
 
     test = commands.Cog(  # Create a new Cog object.
         "Test", "Some commands for testing."  # Give it a name.  # And an optional description.
-    )  # The name and description will be used in the help command.
+    )
+
+    animal = commands.Cog("Animals", "Animal commandz!")
 
     @test.command()
     async def pingcog(ctx):  # No self parameter.
         """Sends Pong!"""
         await ctx.reply("Pong from inside a Cog!")
 
-    @client.command()
+    @animal.command()
     async def cat(ctx):
       """Kat pics!"""
       msg = await ctx.reply("Hav kat pic! (Embd loading... sry 4 waitin :c )", mention=False)
       catto = animals.picture("cat")
       await msg.edit(embed=voltage.SendableEmbed(media=catto), content="Hav kat pic!")
 
-    @client.command()
+    @animal.command()
     async def catfact(ctx):
       """Kat fun facts!! (some may be dark)"""
       await ctx.send(animals.fact("cat"))
 
-    @client.command()
+    @animal.command()
     async def dog(ctx):
       """Dog pics! >:( """
       msg = await ctx.reply("Hav dog pic! (Embd loading... sry 4 waitin :c )", mention=False)
       catto = animals.picture("dog")
       await msg.edit(embed=voltage.SendableEmbed(media=catto), content="Hav dog pic! >:(")
 
-    @client.command()
+    @animal.command()
     async def dogfact(ctx):
       """Dog fun facts!! >:( (some may be dark)"""
       await ctx.send(animals.fact("dog"))
